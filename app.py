01a28916f6c5d1509a053e72a4144bb0a5c60e07
Cauanz <61758981+Cauanz@users.noreply.github.com>
2025-11-12 11:29:40 -0300

    💄 ✨ Implementa funcionalidade de adição e exibição de tarefas no aplicativo TODO em Flask

--- app.py
+++ app.py
@@ -1,18 +1,30 @@
-from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template, request, redirect, url_for
 from models import db
+from models import Task
 
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db' 
+db.init_app(app)
 
-@app.route("/")
+@app.route("/", methods=['POST', 'GET'])
 def index():
 
-  # TODO - PAREI CRIANDO A FUNCIONALIDADE DESSA ROTA (QUE É EXIBIR AS TASKS SE NÃO LEMBRA)
-  # TODO - E FAZER TODO RESTO
+  if(request.method == 'POST'):
+    newTask = Task(content=request.form['content'])
 
-  return render_template("index.html")
+    try:
+      db.session.add(newTask)
+      db.session.commit()
+      return redirect('/')
+    except:
+      print('An exception occurred')
+
+  else:
+    tasks = Task.query.all()
+    return render_template("index.html", tasks=tasks)
+
+# TODO - ADICIONAR OUTRAS ROTAS/MÉTODOS/FUNCIONALIDADES
 
 
 app.run(debug=True)
